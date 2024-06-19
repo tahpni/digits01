73db9f443e35d48b0770288987c27c819b35eeb9
tap <168781414+tahpni@users.noreply.github.com>
2024-06-19 10:02:33 -0400

    Update draw.py

--- draw.py
+++ draw.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import requests as req
 from tkinter import colorchooser, filedialog, ttk
 from matplotlib import pyplot as plt
 import PIL.ImageGrab as ImageGrab
@@ -7,7 +8,7 @@ import os
 class DrawingModule:
     def __init__(self, root):
         self.root = root
-        self.root.title("Drawing Module")
+        self.root.title("Draw a Number")
         self.canvas = tk.Canvas(self.root, bg="white", width=600, height=600)
         self.canvas.pack()
 
@@ -20,6 +21,17 @@ class DrawingModule:
         self.create_widgets()
         self.canvas.bind('<B1-Motion>', self.paint)
         self.canvas.bind('<ButtonRelease-1>', self.stop_paint)
+
+    def on_resize(self,event):
+        # determine the ratio of old width/height to new width/height
+        wscale = float(event.width)/self.width
+        hscale = float(event.height)/self.height
+        self.width = event.width
+        self.height = event.height
+        # resize the canvas 
+        self.config(width=self.width, height=self.height)
+        # rescale all the objects tagged with the "all" tag
+        self.scale("all",0,0,wscale,hscale)
 
     def create_widgets(self):
         self.clear_button = tk.Button(self.root, text="Submit", command=self.submit_canvas)
@@ -38,20 +50,14 @@ class DrawingModule:
         self.old_y = None
 
     def submit_canvas(self):
-       # Save the canvas content as an image file
-        x = self.root.winfo_rootx() + self.canvas.winfo_x()
-        y = self.root.winfo_rooty() + self.canvas.winfo_y()
-        x1 = x + self.canvas.winfo_width()
-        y1 = y + self.canvas.winfo_height()
+        x1 = self.canvas.winfo_rootx()
+        y1 = self.canvas.winfo_rooty()
+        x2 = x1 + self.canvas.winfo_width()
+        y2 = y1 + self.canvas.winfo_height()
 
-        # Grab the image from the canvas and save it
-        ImageGrab.grab().crop((x, y, x1, y1)).save("UserInput.png") 
-    
-        
-        fetch('/execute_draw', { method: 'POST' })
-        then(response) => {response.json()
-        then(data) => {alert(data.output)};
-        }
+        ImageGrab.grab(bbox = (x1, y1, x2, y2)).save("UserInput.png")
+        response = req.post('http://localhost:5000/execute_neural')
+        exit()
 if __name__ == "__main__":
     root = tk.Tk()
     app = DrawingModule(root)
